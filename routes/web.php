a6de9f0387639f3cf1525981733574811356ebbf
GioGreco <grecogiorgio.97@gmail.com>
2022-12-15 19:01:04 +0100

    add comics

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('comics');
+    $comics = config('comicsdb.comics');
+    return view('comics', compact('comics'));
 })->name('comics');
